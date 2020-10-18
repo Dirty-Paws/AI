c282e4cc5fdcd35d0ac31c1a001b4e4b2459a214
whatcanaysu <41306207+whatcanaysu@users.noreply.github.com>
2020-10-18 19:21:27 +0300

    Formating

--- FoodRemainingTimeArduino/dataformating.py
+++ FoodRemainingTimeArduino/dataformating.py
@@ -20,11 +20,11 @@ with open('cokhosverioldu.csv', 'r') as read_obj:
                     dictionary["MamaLevel"].append(data)
             if "Su" in row:
                 dictionary["SuLevel"].append(row.strip(': Suyun Seviyesi\n'))
-                if "celsi" in read_obj.readline():
-                    dictionary["SicaklikLevel"].append(row.strip(' celsius\n'))
+            if "celsi" in row:
+                dictionary["SicaklikLevel"].append(row.strip(' celsius\n'))
 
-                if "cm" in read_obj.readline():
-                    dictionary["UltraLevel"].append(row.strip(' cm\n'))
+            if "cm" in row:
+                dictionary["UltraLevel"].append(row.strip(' cm\n'))
 
 print(dictionary["IsikLevel"])
 print(dictionary["MamaLevel"])
